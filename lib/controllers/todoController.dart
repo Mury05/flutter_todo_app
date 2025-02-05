e386c9bfc684f524bf26b4731ddd24f9cea6829c
Mury05 <murielleodilone@gmail.com>
2025-02-05 15:54:48 +0100

    Add TodoController to manage todo list operations and add function

---
+++ lib/controllers/todoController.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_todo_app/models/todo.dart';
+
+class TodoController {
+  List<Todo> _todosList = [
+    Todo(todo: "Dinner with Jenny"),
+    Todo(todo: "Work on mobile apps for 2 hour"),
+    Todo(todo: "Team Meeting"),
+    Todo(todo: "Check Emails"),
+  ];
+
+  List<Todo> getTodosList() => _todosList;
+
+  void setTodosList({required List<Todo> todoslist}) => _todosList = todoslist;
+
+  void add({required String todo}) {
+    if (todo.isNotEmpty && todo.trim() != '') {
+      List<Todo> newList = getTodosList();
+      newList.insert(0, Todo(todo: todo));
+      setTodosList(todoslist: newList);
+    }
+  }
+}
