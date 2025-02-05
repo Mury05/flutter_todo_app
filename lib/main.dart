3e3fe76cff5879d3c5163343c40263b0937189ec
Mury05 <murielleodilone@gmail.com>
2025-02-05 14:42:25 +0100

    Add Todo list display and enhance home page layout

--- lib/main.dart
+++ lib/main.dart
@@ -66,14 +66,52 @@ class _MyHomePageState extends State<MyHomePage> {
 
       body: Container(
         decoration: BoxDecoration(
-          color: const Color.fromARGB(65, 177, 177, 177),
+          color: const Color.fromARGB(255, 232, 230, 235)
         ),
         child: Center(
           child: Column(
             // mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               // Input Search
-              SearchInput()
+              SearchInput(),
+
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      "All ToDos",
+                      style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: "Arial"),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Todo(todoText: "Dinner with Jenny", checkTodo: false),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Todo(
+                        todoText: "Work on mobile apps for 2 hour",
+                        checkTodo: true),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Todo(todoText: "Team Meeting", checkTodo: false),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Todo(todoText: "Check Emails", checkTodo: false),
+                  ],
+                ),
+              )
             ],
           ),
         ),
@@ -91,6 +129,71 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
+class Todo extends StatelessWidget {
+  final String todoText;
+  final bool checkTodo;
+  const Todo({
+    super.key,
+    required this.todoText,
+    required this.checkTodo,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(16.0),
+      decoration: BoxDecoration(
+          color: Colors.white, borderRadius: BorderRadius.circular(18)),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Checkbox(
+                activeColor: Colors.deepPurple,
+                checkColor: Colors.white,
+                side: BorderSide(color: Colors.deepPurple, width: 2),
+                value: false,
+                onChanged: (bool? newValue) {
+                  newValue = checkTodo;
+                },
+              ),
+              SizedBox(
+                width: 30,
+              ),
+              Container(
+                constraints: BoxConstraints(
+                  maxWidth: MediaQuery.of(context).size.width * 0.5,
+                ),
+                child: Text(
+                  todoText,
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                  overflow: TextOverflow.visible,
+                ),
+              ),
+            ],
+          ),
+          Container(
+            padding: EdgeInsets.all(8.0),
+            decoration: BoxDecoration(
+                color: Colors.red, borderRadius: BorderRadius.circular(5)),
+            child: Icon(
+              Icons.delete,
+              color: Colors.white,
+              size: 20,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
 class SearchInput extends StatelessWidget {
   const SearchInput({
     super.key,
@@ -102,14 +205,14 @@ class SearchInput extends StatelessWidget {
       padding: const EdgeInsets.all(16.0),
       child: TextField(
         decoration: InputDecoration(
-        fillColor: Colors.white,
-        filled: true,
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(30.0),
-          borderSide: BorderSide.none,
-        ),
-        prefixIcon: Icon(Icons.search),
-        hintText: "Search",
+          fillColor: Colors.white,
+          filled: true,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(30.0),
+            borderSide: BorderSide.none,
+          ),
+          prefixIcon: Icon(Icons.search),
+          hintText: "Search",
         ),
       ),
     );
