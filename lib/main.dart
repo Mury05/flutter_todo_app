f96c9c3f894a83fc9b5acfd01a4489a830b39331
Mury05 <murielleodilone@gmail.com>
2025-02-05 12:09:10 +0100

    AppBar and Drawer done ✅

--- lib/main.dart
+++ lib/main.dart
@@ -11,53 +11,72 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Todos',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Todos Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   void _incrementCounter() {
-    setState(() {
-      
-    });
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-     
-        title: Text(widget.title),
+      drawer: Drawer(
+        child: ListView(
+          children: [ListTile(title: Text("Accueil"))],
+        ),
       ),
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(65, 219, 219, 219),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            
+            Container(
+              padding: EdgeInsets.all(2),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.green, width: 3,),
+                borderRadius: BorderRadius.circular(50),
+              ),
+              child: SizedBox(
+                child: Icon(Icons.person, size: 30),
+                  
+              ),
+            ),
+          ],
+        ),
+      ),
+      
       body: Center(
-     
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-          ],
+          children: <Widget>[],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.deepPurple,
         onPressed: _incrementCounter,
         tooltip: 'Add Todo',
-        child: const Icon(Icons.add, color: Colors.white,),
+        child: const Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
