13778e9d11690121377f2e68e754a166f297b4f7
Mury05 <murielleodilone@gmail.com>
2025-02-06 12:27:11 +0100

    Restructuring user model to add email

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,11 +1,12 @@
 class User {
   final String username;
+  final String email;
   final String password;
 
-  User({required this.username, required this.password});
+  User({required this.email, required this.username, required this.password});
 }
 
 final List<User> users = [
-  User(username: "Mury", password: '123'),
-  User(username: "John Doe", password: '123'),
+  User(email: "mury@gmail.com", username: "Mury", password: '123'),
+  User(email: "john@gmail.com", username: "John Doe", password: '123'),
 ];
