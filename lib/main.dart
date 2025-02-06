6ceaf71aebee22026d89a633bd1d030669e3bff9
Mury05 <murielleodilone@gmail.com>
2025-02-06 13:15:10 +0100

    Restructuration du fichier

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_todo_app/routes/routes.dart';
 import 'package:flutter_todo_app/screen/auth/login.dart';
-import 'package:flutter_todo_app/screen/auth/register.dart';
-import 'package:flutter_todo_app/screen/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,12 +19,9 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const LoginPage(),
+      // home: const LoginPage(),
       initialRoute: '/',
-      routes: {
-        '/register' : (context) => const RegisterPage(),
-        '/home': (context) => const MyHomePage()
-      },
+      routes: Routes().getRoutes()
     );
   }
 }
