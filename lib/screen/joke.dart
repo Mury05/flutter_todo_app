50c8ee6ec53aa594802002de332a404bf2301df4
Mury05 <murielleodilone@gmail.com>
2025-02-14 11:19:06 +0100

    Création d'une fonction pour récupérer les données provenant de l'api

--- lib/screen/joke.dart
+++ lib/screen/joke.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class JokePage extends StatefulWidget {
   const JokePage({super.key});
@@ -8,8 +11,40 @@ class JokePage extends StatefulWidget {
 }
 
 class _JokePageState extends State<JokePage> {
+  final String url = "https://official-joke-api.appspot.com/random_joke";
+
+  Future<Map<String, dynamic>> getRandomJoke() async {
+    final response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      print(data);
+      return data;
+    } else {
+      throw Exception('Echec lors de la récupération');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    getRandomJoke();
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                  child: Center(
+                    child: Column(
+                      crossAxisAlignment:
+                          CrossAxisAlignment.center, // Aligne le texte à gauche
+                      children: [const Text("For Joking !!")],
+                    ),
+                  ))
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
